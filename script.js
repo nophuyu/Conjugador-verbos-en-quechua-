1c567334fce86c69300d51652b1ee280d0814c5f
Nicaela Leon <98913905+nophuyu@users.noreply.github.com>
2023-10-10 15:03:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById('conjugateButton').addEventListener('click', conjugateVerb);
+
+function conjugateVerb() {
+    const verbSelect = document.getElementById('verbosSelect');
+    const conjugationResult = document.getElementById('conjugationResult');
+
+    const selectedVerb = verbSelect.value;
+
+    if (!selectedVerb) {
+        conjugationResult.textContent = "Selecciona un verbo.";
+        return;
+    }
+
+    // Insertar la lógica para conjugar el verbo  quechua en presente
+    // ¿?Cómo usar un objeto con las conjugaciones regulares e irregulares.
+
+    // Ejemplo
+    const conjugatedVerb = conjugarVerboEnPresente(selectedVerb);
+
+    conjugationResult.textContent = conjugatedVerb;
+}
+
+function conjugarVerboEnPresente(verbo) {
+    // Escribir  lógica de conjugación del verbo en presente
+    //  Incluir reglas para verbos regulares e irregulares ¿?
+    // Devolver el resultado conjugado
+    return `Ñuqa ${verbo}ni, Qam ${verbo}nki, Pay ${verbo}n, Ñuqanchik ${verbo}nchik, Qamkuna ${verbo}nkichik, Paykuna ${verbo}nku`;
+}
